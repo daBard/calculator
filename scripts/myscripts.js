a9672f978f1b858701a84ca85d651f0eb3934fc1
daBard <50872455+daBard@users.noreply.github.com>
2020-09-17 22:31:29 +0200

    Looks nice, coding next.

---
+++ scripts/myscripts.js
@@ -0,0 +1,30 @@
+// DOM ELEMENTS
+const calcScreen = document.querySelector('#screen');
+const ac = document.querySelector('#ac');
+const ce = document.querySelector('#ce');
+const backspace = document.querySelector('#backspace');
+const division = document.querySelector('#division');
+const seven = document.querySelector('#seven');
+const eigth = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const times = document.querySelector('#times');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const minus = document.querySelector('#minus');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const plus = document.querySelector('#plus');
+const neg_pos = document.querySelector('#neg-pos');
+const zero = document.querySelector('#zero');
+const period = document.querySelector('#period');
+const equals = document.querySelector('#equals');
+
+// GLOBAL VARIABLES
+let entryArray = [];
+let entry = 0;
+let total = 123456789;
+
+calcScreen.innerHTML = total;
+
